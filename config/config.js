cb5cf3fd4761e34a8d3dc796e82761e690a1a2f7
okhan1 <okhan1@swarthmore.edu>
2025-08-27 23:40:22 -0400

    initial stuff added

--- config/config.js
+++ config/config.js
@@ -1,10 +1,9 @@
 
 import profile from './profile.png';
-import { faAppStore, faGithub, faGooglePlay } from '@fortawesome/free-brands-svg-icons';
 import { } from '@fortawesome/free-solid-svg-icons';
 
 export const navigation = {
-  name: "Hashir",
+  name: "Omar",
   links: [
     {
       title: "About",
@@ -21,16 +20,12 @@ export const navigation = {
     {
       title: "Links",
       link: "/links",
-    },
-    {
-      title: "Blog",
-      link: "https://medium.com/@hashirshoaeb",
     }
   ],
 }
 export const intro = {
-  title: "Hey, I'm Hashir",
-  description: "A Computer Engineer creating mobile apps and static websites.",
+  title: "Hey, I'm Omar",
+  description: "A Software Engineer creating Web apps and Lua games.",
   image: profile.src,
   buttons: [
     {
@@ -49,9 +44,7 @@ export const intro = {
 export const about = {
   title: "Who I am",
   description: [
-    "I graduated from National University of Sciences and Technology (NUST) in 2020 with a degree in Computer Engineering. I kick started my professional career as a mobile application developer working remotely for a UK based startup — Lessgo.",
-    "The tech industry is ever-evolving and I love being able to grow with it, while continually solidifying the fundamentals. I opensource my code from a place of empathy - for future developers, teammates, users, and with accessibility in mind. I also love joining communities, helping and mentoring new developers, and supporting organizations to promote diversity in tech. I’m sharing my programming journey on instagram, helping people who are just getting into the space of programming.",
-    "When I’m not programming, I focus on my hobbies which are: diary writing, doodling, meeting people and growing my network.",
+    "I graduated from Swarthmore College in 2025 with a degree in Computer Science and Statistics. I enjoy developing web apps, games, and exploring new technologies. I am currently freelancing as a software developer.",
   ],
 }
 
@@ -59,13 +52,13 @@ export const work = {
   title: "What I do",
   cards: [
     {
-      title: "Mobile App Development",
-      description: "I create pixel perfect iOS and Andriod apps using Flutter.",
+      title: "Software Development",
+      description: "I create web applications using Reactjs and backend services using Nodejs.",
       icons: null,
     },
     {
-      title: "Web Development",
-      description: "I create responsive static websites using Reactjs.",
+      title: "Python Scripting",
+      description: "I write Python scripts to automate engineering tasks and data analysis.",
       icons: null,
     }
   ],
@@ -75,40 +68,23 @@ export const projects = {
   title: "Projects",
   cards: [
     {
-      title: "StarBook",
-      description: "A digital diary and mood tracking app that helps you in keeping track of your mood and productivity throughout the month/year.",
+      title: "PokeStat Info",
+      description: "A Pokemon stats and information website. The website provides detailed information about all the Pokemon, their typing and stats with a search functionality.",
       icons: [
         {
-          icon: faAppStore,
-          link: "https://apps.apple.com/us/app/starbook-journal-ai-diary/id1552418289",
+          link: "https://pokemon-stat-info.netlify.app/",
         },
         {
-          icon: faGithub,
-          link: "https://github.com/hashirshoaeb/star_book",
+          link: "https://github.com/OmarK323/PokeInfo",
         },
       ]
     },
     {
-      title: "QuranTalk",
-      description: "An emotional well being and mental health app. The app helps people to navigate their emotions in the light of the Quran.",
+      title: "Princess of Mars",
+      description: "A Tic-80 retro Lua arcade game. Defeating swarms of enemies on Mars as a princess saving her people.",
       icons: [
         {
-          icon: faAppStore,
-          link: "https://apps.apple.com/us/app/qurantalk/id1563425149",
-        },
-        {
-          icon: faGooglePlay,
-          link: "https://play.google.com/store/apps/details?id=com.ayahemotion.quran_talk",
-        },
-      ]
-    },
-    {
-      title: "Portfolio",
-      description: "A portfolio website template that helps you showcase your work, projects and skills as a software developer or freelancer.",
-      icons: [
-        {
-          icon: faGithub,
-          link: "https://github.com/hashirshoaeb/portfolio",
+          link: "https://github.com/OmarK323/Princess-of-Mars",
         },
       ]
     },
@@ -117,17 +93,12 @@ export const projects = {
 
 export const contact = {
   title: "Get in touch",
-  description: "Coffee Chat! Please do not hesitate to schedule a meeting. Alternatively, feel free to reach out directly by email at hashirshoaeb@gmail.com.",
+  description: "Hit me up if you want to collaborate on a project or just want to say hi.",
   buttons: [
     {
       title: "Email Me",
-      link: "mailto:hashirshoaeb@gmail.com",
+      link: "mailto:omarkhan7@rocketmail.com",
       isPrimary: true,
-    },
-    {
-      title: "Schedule Meeting",
-      link: "https://topmate.io/hashirshoaeb",
-      isPrimary: false,
     },
   ]
 }
@@ -135,35 +106,35 @@ export const contact = {
 // SEARCH ENGINE 
 export const SEO = {
   // 50 - 60 char  
-  title: "Hashir Shoaib | Computer Engineer | Flutter | Reactjs developer",
-  description: "I create mobile apps and static websites. I graduated from National University of Sciences and Technology (NUST) in 2020 with a degree in Computer Engineering.",
+  title: "Omar Khan | Software Developer | Game Developer",
+  description: "I create web apps and Lua games. I graduated from Swarthmore College in 2025 with a degree in Computer Science.",
   image: profile.src,
 }
 
 export const links = {
   image: profile.src,
-  title: "@hashirshoaeb",
-  description: "Computer Engineer | Flutter | Reactjs Developer",
+  title: "@omarkhan",
+  description: "Software Developer | Game Developer",
   cards: [
     {
       title: "My website",
-      link: "https://hashirshoaeb.com/",
+      link: "coming soon",
     },
     {
-      title: "QuranTalk App",
-      link: "https://www.qurantalk.app/",
+      title: "PokeStat Info Website",
+      link: "https://pokemon-stat-info.netlify.app/",
     },
     {
-      title: "StarBook App",
-      link: "https://starbook.dev/",
+      title: "Princess of Mars",
+      link: "Coming Soon",
     },
     {
       title: "My GitHub",
-      link: "https://github.com/hashirshoaeb/",
+      link: "https://github.com/omarK323",
     },
     {
       title: "My LinkedIn",
-      link: "https://www.linkedin.com/in/hashirshoaeb/",
+      link: "https://www.linkedin.com/in/omar-khan-a64204173/",
     },
   ]
 }
